0aafdfb30f0ca46fb1a30eaf6c7167ab4d65b6c1
Murat Nurkassymov <boing416@gmail.com>
2019-05-03 10:47:26 -0700

    Update SoloLearnBasic.java

--- SoloLearnBasic.java
+++ SoloLearnBasic.java
@@ -86,6 +86,14 @@ public class Vehicle {
     this.color = c;
   }
 }
+
+public static void main(String[ ] args) {
+  Vehicle v1 = new Vehicle();
+  v1.setColor("Red");
+  System.out.println(v1.getColor());
+}
+
+//Outputs "Red"
 // Outputs "Woof-Woof"
 
 
