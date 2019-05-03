072dcddb9a54f93be10f21a674330deaa332d4da
Murat Nurkassymov <boing416@gmail.com>
2019-05-03 14:06:03 -0700

    Update SoloLearnBasic.java

--- SoloLearnBasic.java
+++ SoloLearnBasic.java
@@ -697,7 +697,21 @@ public class MyClass {
   }
   }
 }
+//////////////////Writing to Files
+import java.util.Formatter;
 
+public class MyClass {
+   public static void main(String[ ] args) {
+  try {
+    Formatter f = new Formatter("C:\\sololearn\\test.txt");
+    f.format("%s %s %s", "1","John", "Smith \r\n");
+    f.format("%s %s %s", "2","Amy", "Brown");
+    f.close();    
+  } catch (Exception e) {
+    System.out.println("Error");
+  }
+  }
+}
 
 
 
