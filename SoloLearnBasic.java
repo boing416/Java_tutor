cd9b6e7989eb9bf6d7987b7807d867dbd6cd52da
Murat Nurkassymov <boing416@gmail.com>
2019-05-03 10:58:08 -0700

    Update SoloLearnBasic.java

--- SoloLearnBasic.java
+++ SoloLearnBasic.java
@@ -93,8 +93,48 @@ public static void main(String[ ] args) {
   System.out.println(v1.getColor());
 }
 
-//Outputs "Red"
-// Outputs "Woof-Woof"
 
+//****************************Example of a constructor:
+public class Vehicle {
+  private String color;
+  Vehicle() {
+     color = "Red";
+  }
+}
+///Using Constructors
+public class MyClass {
+  public static void main(String[ ] args) {
+    Vehicle v = new Vehicle("Blue");
+  }
+}
+//Constructors
 
+public class Vehicle {
+  private String color;
 
+  Vehicle() {
+    this.setColor("Red");
+  }
+  Vehicle(String c) {
+    this.setColor(c);
+  }
+
+  // Setter
+  public void setColor(String c) {
+    this.color = c;
+  }
+}
+
+/*****************************************MATH******************************************/
+int a = Math.abs(10);  // 10
+int b = Math.abs(-20); // 20
+
+double c = Math.ceil(7.342);  // 8.0
+
+double f = Math.floor(7.343);  // 7.0
+
+int m = Math.max(10, 20);  // 20
+
+int m = Math.min(10, 20);  // 10
+
+double p = Math.pow(2, 3); // 8.0
