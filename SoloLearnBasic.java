e528a594efe213f8eec0d039238abbd1954efbc2
Murat Nurkassymov <boing416@gmail.com>
2019-05-03 10:45:26 -0700

    Update SoloLearnBasic.java

--- SoloLearnBasic.java
+++ SoloLearnBasic.java
@@ -70,8 +70,23 @@ class MyClass {
     dog.bark();
   }
 }
+
+//****************************Getters & Setters
+
+public class Vehicle {
+  private String color;
+
+  // Getter
+  public String getColor() {
+    return color;
+  }
+
+ // Setter
+  public void setColor(String c) {
+    this.color = c;
+  }
+}
 // Outputs "Woof-Woof"
 
 
 
-*/
